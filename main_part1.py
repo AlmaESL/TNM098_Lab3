a6a75c32fd1b7374b632d02e222100264b0be17f
almli825@student.liu.se <almli825@student.liu.se>
2025-04-24 12:37:11 +0200

    featureVec started

--- main_part1.py
+++ main_part1.py
@@ -1,4 +1,8 @@
+import cv2
+import os 
 
+from makeFeatureVector import makeVector 
+from colorConverter import convertToHSV, convertToLAB, convertToYCrCb
 
 # TODO: compute the cosine similarity distance between all vectors 
 
@@ -7,3 +11,9 @@
 # TODO: use the matrix to rank the 11 images in similarity to one chosen image
 
 # TODO: visualize the distance results from the ranking eg using a heatmap on pixel-by-pixel basis
+
+
+# Call images from original image directory 
+original_image_dir = 'C:/Users/almal/Desktop/termin8/TNM098/lab 3/TNM098_Lab3/Lab3.1'
+
+# Extract images 
--- makeFeatureVector.py
+++ makeFeatureVector.py
@@ -1,7 +1,31 @@
-
+from colorConverter import conver_image_color_space
+import statistics as stats
+import cv2
 
 # TODO: decide what features to use
 
 # TODO: extract those features, eg with OpenCV or pillow
 
 # TODO: return the feature vector
+
+# Function to convert to all color spaces
+def convert(img): 
+    # Convert to HSV
+    hsv = conver_image_color_space(img, 'HSV')
+    
+    # Convert to LAB
+    lab = conver_image_color_space(img, 'LAB')
+    
+    # Convert to YCrCb
+    rgb = conver_image_color_space(img, 'RGB')
+    
+    return hsv, lab, rgb
+    
+
+# Help function to split channels 
+def split(img): 
+    c1, c2, c3 = cv2.split(img)
+    return c1, c2, c3
+
+
+# Function to compute the harmonic mean of each image 
