070c736c109f8cc7d8c1c8dd5c2fd39a4aa0653c
almli825@student.liu.se <almli825@student.liu.se>
2025-04-27 16:04:09 +0200

    simple word filtering

---
+++ part2/filter.py
@@ -0,0 +1,17 @@
+from preprocessing import load_and_process
+
+keywords = ['attack', 'terror', 'bomb', 'terrorism' 'dead', 'injured', 'threat', 'violence', 'explosion', 'shooting', 'casualties', 
+            'assault', 'hostage', 'crisis', 'emergency', 'safety', 'security']
+
+def is_relevant(content): 
+    # return any(word in content for word in keywords)
+    # filter on keywords strictly
+    return any(word in content for word in keywords)
+    
+
+def get_relevant(content): 
+
+    content['relevant'] = content['processed_content'].apply(is_relevant)
+    content = content[content['relevant'] == True]
+    return content
+    
--- part2/main_part2.py
+++ part2/main_part2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 from preprocessing import load_and_process
+from filter import get_relevant
 
 # Filepath
 filepath = 'C:/Users/almal/Desktop/termin8/TNM098/lab 3/TNM098_Lab3/TNM098-MC3-2011.csv'
@@ -14,5 +15,16 @@ df = load_and_process(filepath)
 # Temporal Distribution (Unfiltered)
 df['Date'] = pd.to_datetime(df['Date'])
 
-fig = px.histogram(df, x='Date', nbins=50, title='News Report Frequency')
+# fig = px.histogram(df, x='Date', nbins=50, title='News Report Frequency')
+fig = px.bar(df, x='Date', title='News Report Frequency')
+fig.show()
+
+
+# Get relevant data
+relevant_df = get_relevant(df)
+# print('length of relevant_df:', len(relevant_df))
+# print('length of df:', len(df))
+
+# Temporal Distribution (Filtered)
+fig = px.bar(relevant_df, x='Date', title='News Report Frequency - Filtered')
 fig.show()
