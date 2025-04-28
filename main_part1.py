c61d812c21f2a38a6144c066c26f724660c45fa0
almli825@student.liu.se <almli825@student.liu.se>
2025-04-28 16:15:19 +0200

    todos removed

--- main_part1.py
+++ main_part1.py
@@ -9,8 +9,8 @@ from makeFeatureVector import makeFeatureVec
 # === MAIN ===
 
 if __name__ == "__main__":
-    image_folder = "Lab3.1"
-    #image_folder = 'C:/Users/almal/Desktop/termin8/TNM098/lab 3/TNM098_Lab3/Lab3.1'
+    # image_folder = "Lab3.1"
+    image_folder = 'C:/Users/almal/Desktop/termin8/TNM098/lab 3/TNM098_Lab3/Lab3.1'
 
     image_paths = sorted([os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith(('.jpg', '.png'))])[:12]
 
@@ -27,11 +27,11 @@ if __name__ == "__main__":
     # compute the cosine similarity distance between all vectors
     similarity_matrix = cosine_similarity(feature_matrix)
 
-    # TODO: save results in a 12x12 matrix of cosine similarity distances
+    # save results in a 12x12 matrix of cosine similarity distances
     print("12x12 Cosine Similarity Matrix:")
     print(similarity_matrix)
 
-    # TODO: use the matrix to rank the 11 images in similarity to one chosen image
+    # use the matrix to rank the 11 images in similarity to one chosen image
     chosen_index = 0  # välj t.ex. första bilden
     scores = similarity_matrix[chosen_index]
     ranked_indices = np.argsort(scores)[::-1]
